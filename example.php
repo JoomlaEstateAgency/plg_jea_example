258d38dd699976dbea962c6b3433dde5eaccd382
Sylvain Philip <contact@sphilip.com>
2012-04-07 13:10:44 +0000

    Plugins dpe and example migration

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 
+ *
  * @package		Joomla
  * @subpackage	JEA
  * @copyright	Copyright (C) 2011 PHILIP Sylvain. All rights reserved.
@@ -9,7 +9,7 @@
  * GNU General Public License, and as distributed it includes or is derivative
  * of works licensed under the GNU General Public License or other free or open
  * source software licenses.
- * 
+ *
  */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
@@ -26,125 +26,138 @@ jimport('joomla.event.plugin');
 class plgJeaExample extends JPlugin
 {
 
-	/**
-	 * Constructor
-	 *
-	 * For php4 compatability we must not use the __constructor as a constructor for plugins
-	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-	 * This causes problems with cross-referencing necessary for the observer design pattern.
-	 *
-	 * @param object $subject The object to observe
-	 * @param object $params  The object that holds the plugin parameters
-	 * @since 1.5
-	 */
-	function plgJeaExample( &$subject, $params )
-	{
-		parent::__construct( $subject, $params );
-	}
-    
-    
     /**
-     * onInitTableProperty Method
-     * This method is called just before $row property initialization.
-     * It should be usefull to add fields on the fly
-     * 
-     * @param TableProperties $table
-     * @return void;
+     * Constructor
+     *
+     * For php4 compatability we must not use the __constructor as a constructor for plugins
+     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+     * This causes problems with cross-referencing necessary for the observer design pattern.
+     *
+     * @param object $subject The object to observe
+     * @param object $params  The object that holds the plugin parameters
+     * @since 1.5
      */
-    function onInitTableProperty(&$table)
+    function plgJeaExample( &$subject, $params )
     {
-        
+        parent::__construct( $subject, $params );
     }
-    
+
+
     /**
      * onBeforeSaveProperty method
-     * 
+     *
      * Called before $row property save.
      *
+     * @param string $namespace
      * @param TableProperties $row
      * @param boolean $is_new
      * @return boolean
      */
-    function onBeforeSaveProperty(&$row, $is_new)
+    function onBeforeSaveProperty($namespace, &$row, $is_new)
     {
         return true;
     }
-    
-	/**
+
+    /**
      * onAfterSaveProperty method
-     * 
+     *
      * Called after $row property save.
      *
+     * @param string $namespace
      * @param TableProperties $row
      * @param boolean $is_new
      * @return void
      */
-    function onAfterSaveProperty(&$row, $is_new)
+    function onAfterSaveProperty($namespace, &$row, $is_new)
     {
-        
+
     }
-    
-    
-    
-    
-    
-	/**
-     * onAfterStartPane method (Called in the admin property form)
+
+    /**
+     * onAfterLoadPropertyForm method
      *
-     * @param JPane $pane
-     * @param TableProperties $row
+     * Called after load the property form.
+     *
+     * @param JForm $form
+     * @return void
      */
-    function onAfterStartPane(&$pane, &$row)
+    function onAfterLoadPropertyForm(&$form)
     {
-        echo $pane->startPanel( JText::_('My new panel 1') , "my-new-panel-1" );
-        echo '<p>My new panel 1</p>';
-        echo $pane->endPanel();
+
+    }
+
+    /**
+     * onAfterStartPanels method (Called in the admin property form)
+     *
+     * @param TableProperties $row
+     * @return void
+     */
+    function onAfterStartPanels(&$row)
+    {
+        echo JHtml::_('sliders.panel', 'Exemple start panel', 'test-start-panel');
+        echo '<fieldset class="panelform">Test start panel</fieldset>';
     }
 
     /**
      * onBeforeEndPane method (Called in the admin property form)
      *
-     * @param JPane $pane
      * @param TableProperties $row
+     * @return void
      */
-    function onBeforeEndPane(&$pane, &$row)
+    function onBeforeEndPanels(&$row)
     {
-        echo $pane->startPanel( JText::_('My new panel 2') , "my-new-panel-2" );
-        echo '<p>My new panel 2</p>';
-        echo $pane->endPanel();
+        echo JHtml::_('sliders.panel', 'Exemple end panel', 'test-start-panel');
+        echo '<fieldset class="panelform">Test end panel</fieldset>';
     }
-    
-    
+
     /**
-     * onBeforeShowDescription method 
-     * Called in the default_item.php tpl
+     * onBeforeSearch method (Called before query properties list in frontend)
+     *
+     * @param JDatabaseQueryElement $query
+     * @param JObject $Modelstate
+     *
+     * @return void
+     */
+    function onBeforeSearch(&$query, &$Modelstate)
+    {
+
+    }
+
+    /**
+     * onBeforeShowDescription method
+     * Called in the defaul.php property layout
      *
      * @param stdClass $row
      */
     function onBeforeShowDescription(&$row)
     {
-        
         echo '<h3>onBeforeShowDescription</h3>';
     }
-    
-    
-	/**
-     * onAfterShowDescription method 
-     * Called in the default_item.php tpl
+
+    /**
+     * onAfterShowDescription method
+     * Called in the defaul.php property layout
      *
      * @param stdClass $row
      */
     function onAfterShowDescription(&$row)
     {
-        
+
         echo '<h3>onAfterShowDescription</h3>';
     }
-    
 
-    
-    
-    
-    
+    /**
+     * onBeforeSendContactForm method
+     *
+     * Called before emailing the contact form
+     *
+     * @param array $data
+     * @return boolean true on success or false
+     */
+    function onBeforeSendContactForm($data)
+    {
+        return true;
+    }
 
 
 }
